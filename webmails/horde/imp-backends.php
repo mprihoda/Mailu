7bb9a83249522a5494b5845afc15f4c6fca48fd4
Michal Prihoda <michal@prihoda.net>
2018-05-23 10:11:56 +0200

    Use front to connect to IMAP to make authentication work properly.

--- webmails/horde/imp-backends.php
+++ webmails/horde/imp-backends.php
@@ -4,14 +4,14 @@
         // ENABLED by default; will connect to IMAP port on local server
         'disabled' => false,
         'name' => 'IMAP Server',
-        'hostspec' => 'imap',
+        'hostspec' => 'front',
         'hordeauth' => 'full',
         'protocol' => 'imap',
-        'port' => 143,
+        'port' => 993,
         // Plaintext logins are disabled by default on IMAP servers (see RFC 3501
         // [6.2.3]), so TLS is the only guaranteed authentication available by
         // default.
         // 'secure' => 'tls',
-        'secure' => false,
+        'secure' => 'ssl',
     );
 ?>
